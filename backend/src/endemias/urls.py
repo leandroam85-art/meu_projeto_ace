87e511ad6c09a6af77fd13709d4838bf8455b09a
Leandro Mendes <seu_email@exemplo.com>
2026-03-15 22:10:42 -0300

    Adicionando endpoint de login do app via token

--- backend/src/endemias/urls.py
+++ backend/src/endemias/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
+from . import views
 
-# Aqui ficarão APENAS as rotas da API do celular. 
-# NUNCA coloque o admin.site.urls ou o dashboard aqui dentro!
 urlpatterns = [
-    # path('visitas/', views.alguma_view_de_api),
+    # A PORTA DE ENTRADA DO APLICATIVO: 
+    # Recebe usuário e senha e devolve o Token de segurança
+    path('login/', obtain_auth_token, name='api_login'),
+    
+    # (No futuro, colocaremos aqui a rota que recebe os focos de dengue)
+    # path('visitas/', views.receber_visita, name='receber_visita'),
 ]
